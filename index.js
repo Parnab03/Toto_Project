0a80d473310d96ebb492044fb844ce6c596287c1
Kiran Samanta <119595085+Kiran1126@users.noreply.github.com>
2022-12-26 23:53:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var y = document.getElementById("bas");
+var x = document.getElementById("bfh");
+var z = document.getElementById("btn-container");
+
+function bas() {
+    x.style.left = "-800px"
+    y.style.left = "130px"
+    z.style.left = "0"
+}
+function bfh() {
+    x.style.left = "130px"
+    y.style.left = "850px"
+    z.style.left = "0"
+}
